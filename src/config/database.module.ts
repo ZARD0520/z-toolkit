9abe852ff698a519583839dfb480f2eeec156d24
ZARD0520 <422451702@qq,com>
2024-06-13 17:59:16 +0800

    init

---
+++ src/config/database.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env' + (process.env.NODE_ENV ? '.' + process.env.NODE_ENV : '')
+    }),
+  ],
+})
+export class DatabaseModule { }
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter'
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api')
+  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalInterceptors(new TransformInterceptor())
+  await app.listen(3000);
+}
+bootstrap();
