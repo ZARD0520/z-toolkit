7e37987d5a54f2798150c9bfc7a69c5e79bf42ad
huangjunjie <huangjunjie@newlang.cn>
2025-07-08 18:18:47 +0800

    update

--- src/config/database/mongodb.config.ts
+++ src/config/database/mongodb.config.ts
@@ -1,7 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('mongo', () => ({
-  uri: process.env.mongo_uri || 'mongodb://localhost/nest', // 默认连接字符串
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  uri: process.env.MONGO_URI || 'mongodb://localhost/nest', // 默认连接字符串
 }));
--- src/config/database/mysql.config.ts
+++ src/config/database/mysql.config.ts
@@ -1,11 +1,11 @@
 export default () => ({
   database: {
     type: 'mysql',
-    host: process.env.sql_host || 'localhost',
-    port: (process.env.sql_port && parseInt(process.env.sql_port, 10)) || 3306,
+    host: process.env.SQL_HOST || 'localhost',
+    port: (process.env.SQL_PORT && parseInt(process.env.SQL_PORT, 10)) || 3306,
     username: process.env.SQL_USERNAME || 'root',
-    password: process.env.sql_password || 'password',
-    database: process.env.sql_database || 'test1',
+    password: process.env.SQL_PASSWORD || 'password',
+    database: process.env.SQL_DATABASE || 'test1',
     entities: [__dirname + '/../../**/**/*.entity{.ts,.js}'],
     synchronize: process.env.NODE_ENV === 'development', // 仅在开发环境下同步
   },
