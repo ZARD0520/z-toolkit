42b70a18578efc24cd6ce609b6d9bdbe1c0ea3fc
ZARD0520 <422451702@qq.com>
2025-07-18 21:23:21 +0800

    update：调整监听地址

--- src/main.ts
+++ src/main.ts
@@ -51,6 +51,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
 
-  await app.listen(80);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
