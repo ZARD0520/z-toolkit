a0f0bdcef0dacd67a5878c019d13c4c5a3ddd31e
zard <422451702@qq.com>
2025-12-22 18:29:14 +0800

    update

--- src/config/database/mongodb.config.ts
+++ src/config/database/mongodb.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('mongo', () => ({
-  uri: process.env.MONGO_URI || 'mongodb://mongo-container/nest', // 默认连接字符串
+  uri: process.env.MONGO_URI || '', // 默认连接字符串
   username: process.env.MONGO_USERNAME,
   password: process.env.MONGO_PASSWORD,
 }));
